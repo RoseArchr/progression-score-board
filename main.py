b0916e5c4ae4bf34ee6f2ad190d8a07a46d8a465
RoseArchr <rosekelley@gmail.com>
2026-08-16 18:03:27 -0500

    loop functionality until user is done

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import json
 import os
-from typing import Any
 
-#player1 = "Rose"
-#player2 = "Lily"
-
-def win(player: str, player_scores: dict[str, int]):
+def win(player: str, player_scores: dict[str, int]) -> dict[str, int]:
     player_scores[player] += 1
+    save(player_scores)
+    return player_scores
 
 def save(player_scores: dict[str, int]):
     with open("data.json", "w") as file:
@@ -28,7 +26,27 @@ def new_game():
         player_scores[str(input())] = 0
     save(player_scores)
 
+def play(scores: dict[str, int]):
+    in_progress = True
+    while in_progress:
+        print("Enter winner (or done to end):")
+        i = str(input())
+        if i == "done":
+            in_progress = False
+        elif i == "clear":
+            print("starting a new game")
+            new_game()
+            in_progress = False
+            play(load())
+        elif i in scores:
+            scores = win(i, scores)
+            print(load())
+        else:
+            print("Invalid input")
+
+
 def main():
+    player_scores = {}
     if is_file_empty():
         print("no game data found, Creating new game.")
         new_game()
@@ -37,5 +55,7 @@ def main():
         print("game data found")
         player_scores = load()
         print(player_scores)
+
+    play(player_scores)
 
 main()
